6bbba9ffac00c4d01d15993e3572e72bf5241597
Moruf <hellokmgmedia@gmail.com>
2025-11-09 16:33:09 +0100

    ADD: Product images for Audio Devices (Bluetooth Speaker Mini, Noise-Cancelling Headphones) and Power & Charging (Power Bank, Wireless Charging Pad, Portable Solar Charger)

--- api/product_data/images.py
+++ api/product_data/images.py
@@ -4,6 +4,7 @@ Contains Cloudinary image URLs for all products
 """
 
 PRODUCT_IMAGES = {
+    # WEARABLES & FITNESS
     "Smartwatch X": [
         "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762691226/Sales%20Chatbot%20Images/For%20Telegram/Smartwatch%20X/Gemini_Generated_Image_k6xnflk6xnflk6xn_pgg3u6.png",
         "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762691224/Sales%20Chatbot%20Images/For%20Telegram/Smartwatch%20X/Gemini_Generated_Image_u8qck5u8qck5u8qc_1_femthv.png",
@@ -25,11 +26,44 @@ PRODUCT_IMAGES = {
         "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762692902/Sales%20Chatbot%20Images/For%20Telegram/Wireless%20Earbuds/_Image_xu5yhpxu5yhpxu5y_okppjt.png",
         "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762692905/Sales%20Chatbot%20Images/For%20Telegram/Wireless%20Earbuds/_Image_p81plhp81plhp81p_rnlzkl.png"
     ],
+    
+    # AUDIO DEVICES
+    "Bluetooth Speaker Mini": [
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762695593/Sales%20Chatbot%20Images/For%20Telegram/Bluetooth%20Speaker%20Mini/_Image_lesgkmlesgkmlesg_ldqkff.png",
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762695591/Sales%20Chatbot%20Images/For%20Telegram/Bluetooth%20Speaker%20Mini/_Image_9vmzke9vmzke9vmz_ryqo8b.png",
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762695587/Sales%20Chatbot%20Images/For%20Telegram/Bluetooth%20Speaker%20Mini/_Image_bjwjxjbjwjxjbjwj_obdvkz.png",
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762695603/Sales%20Chatbot%20Images/For%20Telegram/Bluetooth%20Speaker%20Mini/_Image_1640a11640a11640_myj036.png"
+    ],
+    "Noise-Cancelling Headphones": [
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762698905/Sales%20Chatbot%20Images/For%20Telegram/Noise-Cancelling%20Headphones/_Image_nb4vodnb4vodnb4v_eyb8hm.png",
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762698909/Sales%20Chatbot%20Images/For%20Telegram/Noise-Cancelling%20Headphones/_Image_9hvp439hvp439hvp_nqb8m2.png",
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762698903/Sales%20Chatbot%20Images/For%20Telegram/Noise-Cancelling%20Headphones/_Image_ti1we4ti1we4ti1w_deblac.png"
+    ],
+    
+    # POWER & CHARGING
+    "Power Bank 20000mAh": [
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762700111/Sales%20Chatbot%20Images/For%20Telegram/POWER%20CHARGING/_Image_x545emx545emx545_twz1mz.png",
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762700118/Sales%20Chatbot%20Images/For%20Telegram/POWER%20CHARGING/_Image_vhl2a5vhl2a5vhl2_d32agv.png",
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762700220/Sales%20Chatbot%20Images/For%20Telegram/POWER%20CHARGING/_Image_9eekdl9eekdl9eek_a6iukl.png"
+    ],
+    "Wireless Charging Pad": [
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762700891/Sales%20Chatbot%20Images/For%20Telegram/Wireless%20Charging%20Pad/_Image_5cv4ir5cv4ir5cv4_opv4qc.png",
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762700888/Sales%20Chatbot%20Images/For%20Telegram/Wireless%20Charging%20Pad/_Image_pv6edcpv6edcpv6e_pzunsw.png",
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762700885/Sales%20Chatbot%20Images/For%20Telegram/Wireless%20Charging%20Pad/_Image_g5gv8jg5gv8jg5gv_rdaac6.png"
+    ],
+    "Portable Solar Charger": [
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762701563/Sales%20Chatbot%20Images/For%20Telegram/Portable%20Solar%20Charger/_Image_sidlj1sidlj1sidl_coedyk.png",
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762701567/Sales%20Chatbot%20Images/For%20Telegram/Portable%20Solar%20Charger/_Image_izuonkizuonkizuo_nxyoki.png",
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762701565/Sales%20Chatbot%20Images/For%20Telegram/Portable%20Solar%20Charger/_Image_n26avcn26avcn26a_vr22ri.png"
+    ],
+    
+    # SMART HOME
     "Smart LED Strip Lights": [
         "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762687130/Sales%20Chatbot%20Images/For%20Telegram/Smart%20LED%20Strip%20Lights/Smart_LED_Strip_Lights_lmmvrr.png",
         "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762687132/Sales%20Chatbot%20Images/For%20Telegram/Smart%20LED%20Strip%20Lights/Smart_LED_Strip_Lights1_jbxxwt.png",
         "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762687133/Sales%20Chatbot%20Images/For%20Telegram/Smart%20LED%20Strip%20Lights/Smart_LED_Strip_Lights2_cgen2j.png",
         "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762687134/Sales%20Chatbot%20Images/For%20Telegram/Smart%20LED%20Strip%20Lights/Smart_LED_Strip_Lights3_gag9xw.png"
     ],
+    
     # Add more products as you upload their images to Cloudinary
 }
