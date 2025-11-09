df4a8d6b9a050c83cbab311ec49eb87f50a2548a
Moruf <hellokmgmedia@gmail.com>
2025-11-09 14:01:33 +0100

    ADD: Product images for Smartwatch X, Fitness Tracker Band, Fitness Band Pro, and Wireless Earbuds Pro

--- api/product_data/images.py
+++ api/product_data/images.py
@@ -4,6 +4,27 @@ Contains Cloudinary image URLs for all products
 """
 
 PRODUCT_IMAGES = {
+    "Smartwatch X": [
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762691226/Sales%20Chatbot%20Images/For%20Telegram/Smartwatch%20X/Gemini_Generated_Image_k6xnflk6xnflk6xn_pgg3u6.png",
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762691224/Sales%20Chatbot%20Images/For%20Telegram/Smartwatch%20X/Gemini_Generated_Image_u8qck5u8qck5u8qc_1_femthv.png",
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762691223/Sales%20Chatbot%20Images/For%20Telegram/Smartwatch%20X/Gemini_Generated_Image_u8qck5u8qck5u8qc_vd1kt7.png",
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762691228/Sales%20Chatbot%20Images/For%20Telegram/Smartwatch%20X/Gemini_Generated_Image_c8r2bdc8r2bdc8r2_v5rzgc.png"
+    ],
+    "Fitness Tracker Band": [
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762691610/Sales%20Chatbot%20Images/For%20Telegram/Fitness%20Tracker%20Band/Gemini_Generated_Image_8cajvk8cajvk8caj_gudtlm.png",
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762691607/Sales%20Chatbot%20Images/For%20Telegram/Fitness%20Tracker%20Band/Gemini_Generated_Image_wz3px7wz3px7wz3p_1_ilvqub.png",
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762691609/Sales%20Chatbot%20Images/For%20Telegram/Fitness%20Tracker%20Band/Gemini_Generated_Image_wz3px7wz3px7wz3p_ovicol.png"
+    ],
+    "Fitness Band Pro": [
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762692128/Sales%20Chatbot%20Images/For%20Telegram/Fitness%20Band%20Pro/_Image_16rbv816rbv816rb_tp16us.png",
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762692127/Sales%20Chatbot%20Images/For%20Telegram/Fitness%20Band%20Pro/_Image_vkit0uvkit0uvkit_g6332t.png",
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762692127/Sales%20Chatbot%20Images/For%20Telegram/Fitness%20Band%20Pro/_Image_m5eksjm5eksjm5ek_h5hjhr.png"
+    ],
+    "Wireless Earbuds Pro": [
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762692905/Sales%20Chatbot%20Images/For%20Telegram/Wireless%20Earbuds/_Image_hdpashhdpashhdpa_gjq05u.png",
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762692902/Sales%20Chatbot%20Images/For%20Telegram/Wireless%20Earbuds/_Image_xu5yhpxu5yhpxu5y_okppjt.png",
+        "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762692905/Sales%20Chatbot%20Images/For%20Telegram/Wireless%20Earbuds/_Image_p81plhp81plhp81p_rnlzkl.png"
+    ],
     "Smart LED Strip Lights": [
         "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762687130/Sales%20Chatbot%20Images/For%20Telegram/Smart%20LED%20Strip%20Lights/Smart_LED_Strip_Lights_lmmvrr.png",
         "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762687132/Sales%20Chatbot%20Images/For%20Telegram/Smart%20LED%20Strip%20Lights/Smart_LED_Strip_Lights1_jbxxwt.png",
@@ -11,11 +32,4 @@ PRODUCT_IMAGES = {
         "https://res.cloudinary.com/ds2h3iwys/image/upload/v1762687134/Sales%20Chatbot%20Images/For%20Telegram/Smart%20LED%20Strip%20Lights/Smart_LED_Strip_Lights3_gag9xw.png"
     ],
     # Add more products as you upload their images to Cloudinary
-    # Format:
-    # "Product Name": [
-    #     "cloudinary_url_1",
-    #     "cloudinary_url_2",
-    #     "cloudinary_url_3",
-    #     "cloudinary_url_4"
-    # ],
 }
